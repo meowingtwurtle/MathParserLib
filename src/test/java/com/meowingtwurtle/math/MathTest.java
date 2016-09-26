87026b5bf42d2e29d5a3b66eabc1a4e71e12ca4b
MeowingTwurtle <jason.e.cobb@gmail.com>
2016-09-25 22:26:42 -0400

    Convert to Maven

---
+++ src/test/java/com/meowingtwurtle/math/MathTest.java
@@ -0,0 +1,36 @@
+package com.meowingtwurtle.math;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MathTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MathTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MathTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
