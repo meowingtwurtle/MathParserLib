65ecf898276aa9720f15350d64811777176ef0a5
MeowingTwurtle <jason.e.cobb@gmail.com>
2016-10-08 13:00:00 -0400

    Create actual tests

--- src/test/java/com/meowingtwurtle/math/MathTest.java
+++ src/test/java/com/meowingtwurtle/math/MathTest.java
@@ -1,8 +1,11 @@
 package com.meowingtwurtle.math;
 
+import com.meowingtwurtle.math.api.IMathHandler;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.math.BigDecimal;
 
 /**
  * Unit test for simple App.
@@ -21,16 +24,14 @@ public class MathTest extends TestCase {
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
+    public static Test suite() {
         return new TestSuite( MathTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertEquals(BigDecimal.valueOf(0.0), IMathHandler.getMathHandler().parse("sin(0)").eval());
+        assertEquals(BigDecimal.valueOf(0.0), IMathHandler.getMathHandler().parse("sin(1+sin(0)-1)").eval());
+        assertEquals(BigDecimal.valueOf(0.0), IMathHandler.getMathHandler().parse("(1 + sin(1+sin(0)-1) - 1)").eval());
+        assertEquals(BigDecimal.valueOf(0.0), IMathHandler.getMathHandler().parse("2*(1 + sin(1+sin(0)-1) - 1)").eval());
     }
 }
